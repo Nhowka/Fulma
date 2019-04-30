332a26a87f68f6e66cdd18c4dd6849a7ce33f622
Björn <bo@kbct.de>
2019-04-30 11:34:30 +0000

    date picker: use react hooks for ui state + add support for alternative renders
    Fixes: #101

--- src/Fulma.Elmish/DatePicker/Types.fs
+++ src/Fulma.Elmish/DatePicker/Types.fs
@@ -1,9 +1,15 @@
 namespace Fulma.Elmish.DatePicker
 
 open System
+open Fable.React
 open Fable.React.Props
 
 module Types =
+
+    type Elements =
+        { Input: ReactElement
+          DeleteButton: ReactElement option
+          Calendar: ReactElement option }
 
     type State =
         { Today : DateTime option
@@ -24,7 +30,7 @@ module Types =
           ShowDeleteButton = false  }
 
     type Config<'Msg> =
-        { OnChange : State * (DateTime option) -> 'Msg
+        { OnChange : DateTime option -> 'Msg
           Local : Date.Local.Localization
           DatePickerStyle : CSSProp list }
 
